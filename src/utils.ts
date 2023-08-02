e7f1731cb11c69288dd4a5faaeddf37869cd5fe1
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2023-08-02 14:53:29 +1000

    1.1.4. Fix header bug with missing key for empty string value

--- src/utils.ts
+++ src/utils.ts
@@ -8,9 +8,9 @@ export const generateQueryString = (qs: Record<string, string | number>): string
 
 export const parseHeaders = (headerLines: string[]): OutgoingHttpHeaders => {
   return headerLines.reduce((acc, header) => {
-    const [name, value] = header.split(':');
-    if (name && value) {
-      acc[name.trim()] = value.trim();
+    const [name, ...values] = header.split(':');
+    if (name && values.length > 0) {
+      acc[name.trim()] = values.join(':').trim();
     }
     return acc;
   }, {} as OutgoingHttpHeaders);
