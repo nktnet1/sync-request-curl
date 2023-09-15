9da8b59b4d6c7a558355703a4d55ee97a779fbf1
Khiet Tam Nguyen <nktnet999@gmail.com>
2023-09-15 20:09:38 +1000

    Fixed minimum bound for curlcode error to be 1 instead of 0

--- src/errors.ts
+++ src/errors.ts
@@ -4,7 +4,7 @@ export class CurlError extends Error {
   constructor(code: number, message: string) {
     super(message);
     if (code < 1 || code > 99) {
-      throw new Error(`CurlError code must be between 0 and 99. Given: ${code}`);
+      throw new Error(`CurlError code must be between 1 and 99. Given: ${code}`);
     }
     this.code = code;
     Object.setPrototypeOf(this, CurlError.prototype);
