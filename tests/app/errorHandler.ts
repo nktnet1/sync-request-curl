fc1b40535b864f6ce63e5769604cb70fa8195395
Khiet Tam Nguyen <nktnet999@gmail.com>
2023-10-08 18:28:17 +1100

    Docstring added for errorHandler

--- tests/app/errorHandler.ts
+++ tests/app/errorHandler.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
-const errorHandler = () => (err: any, req: Request, res: Response, next: NextFunction) => {
+/**
+ * Express error handler middleware.
+ * - https://expressjs.com/en/guide/error-handling.html
+ */
+const errorHandler = () => (err: any, _req: Request, res: Response, next: NextFunction) => {
   const statusCode = err.status ?? err.statusCode ?? 500;
   console.log(statusCode === 500 ? err : err.message);
   res.status(statusCode).json({ error: err.message });
