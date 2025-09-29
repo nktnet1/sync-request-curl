8ec42a1b1a91976720978b2f829afccb445df412
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-09-29 12:22:20 +1000

    test: explicitly list include/exclude files for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globalSetup: ['./tests/globalSetup.ts'],
+    coverage: {
+      include: ['src/**/*.ts'],
+      exclude: ['src/types.ts'],
+    },
   }
 });
