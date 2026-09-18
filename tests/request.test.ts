424b3e65f6ef936deaf6e59f19183a4362238249
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-09-18 16:31:26 +1000

    fix: getBody() for buffer type

--- tests/request.test.ts
+++ tests/request.test.ts
@@ -336,8 +336,10 @@ describe("res.getBody()", () => {
 describe("Response buffering", () => {
   test("large response assembled across multiple chunks", () => {
     const res = request("GET", `${SERVER_URL}/large/response`);
-    expect(res.body.length).toStrictEqual(512 * 1024);
-    expect(res.body.equals(Buffer.alloc(512 * 1024, "x"))).toStrictEqual(true);
+    expect(res.getBody().length).toStrictEqual(512 * 1024);
+    expect(res.getBody().equals(Buffer.alloc(512 * 1024, "x"))).toStrictEqual(
+      true,
+    );
   });
 });
 
