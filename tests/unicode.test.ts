def9894c2fa15263c167d473ccf9d63e1d877d40
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-09-25 10:42:37 +1000

    fix: unicode const type

--- tests/unicode.test.ts
+++ tests/unicode.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import request, { type HttpVerb } from "#/index";
+import request from "#/index";
 import { SERVER_URL } from "./app/config";
 
 describe.each([
@@ -13,10 +13,10 @@ describe.each([
   "こんにちは, नमस्ते, مرحبًا, 你好, Γειά σας, שלום, Привет, გამარჯობა",
 ])("Unicode characters: %s", (emoji) => {
   test.each([
-    { method: "GET", route: "/get", key: "qs" },
-    { method: "POST", route: "/post", key: "json" },
+    { method: "GET", route: "/get", key: "qs" } as const,
+    { method: "POST", route: "/post", key: "json" } as const,
   ])("Method=$method, route=$route, key=$key", ({ method, route, key }) => {
-    const res = request(method as HttpVerb, `${SERVER_URL + route}`, {
+    const res = request(method, `${SERVER_URL + route}`, {
       [key]: { value: emoji },
     });
     expect(JSON.parse(res.body.toString())).toStrictEqual({ value: emoji });
